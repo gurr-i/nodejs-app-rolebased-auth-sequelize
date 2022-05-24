249f80e639487b9c9e105b67ffb70b2a1bcb95c8
Gurveer <gopal@ph.iitr.ac.in>
2022-05-24 19:08:52 +0530

    update

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: 'http://example.com',
+  origin: "http://localhost:8081",
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
